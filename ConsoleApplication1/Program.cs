1233c82db29d3280982dea45a3d91f1cc1e8868c
saikr <saikr@DESKTOP-I95LPBK>
2016-08-14 00:23:28 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int columns = 10;
+            for (int i=0; i<columns/2; i++)
+            {
+                for(int j=0; j<columns; j++)
+                {
+                    if(j>=columns/2-i && j<=columns/2 + i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
